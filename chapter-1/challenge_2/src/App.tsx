a777cc3edd43da8d4d31bf4b0d127af5987cbeaf
Ruan Tiengo <ruantiengo@gmail.com>
2022-03-19 07:06:18 -0300

    [add] c1-d2 completed

---
+++ chapter-1/challenge_2/src/App.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+import { Button } from "./components/Button";
+import { MovieCard } from "./components/MovieCard";
+
+// import { SideBar } from './components/SideBar';
+// import { Content } from './components/Content';
+
+import { api } from "./services/api";
+
+import "./styles/global.scss";
+
+import "./styles/sidebar.scss";
+import "./styles/content.scss";
+import { SideBar } from "./components/SideBar";
+import { Content } from "./components/Content";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <SideBar
+        selectedGenreId={selectedGenreId}
+        setSelectedGenreId={setSelectedGenreId}
+      />
+      <Content selectedGenreId={selectedGenreId} />
+    </div>
+  );
+}
